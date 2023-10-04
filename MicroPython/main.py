f63b5020177c55198be62a561dcf000a0a3f2223
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2023-10-04 17:09:36 +0000

    py vers

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,21 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Liya Getachew
+Created on: Oct 2023
+This module is a Micro:bit MicroPython program that adds a point everytime button a is clicked.
 """
 
 from microbit import *
 
+display.clear()
+display.show(Image.HEART)
 
-display.scroll("Hello, World!")
+# variable
+number_of_cookies_clicked = 0
+
+while True:
+    if button_a.is_pressed():
+        number_of_cookies_clicked = number_of_cookies_clicked + 1
+        display.scroll(str(number_of_cookies_clicked))
+    if button_b.is_pressed():
+        number_of_cookies_clicked = 0
+        display.scroll(str(number_of_cookies_clicked))
